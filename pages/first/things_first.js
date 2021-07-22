6a2f91d59c1bf5450f783d5408744a20287d921c
Ulisses <m.stunik@gmail.com>
2021-07-22 16:39:05 -0300

    initial commit

---
+++ pages/first/things_first.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Button from "../../components/Button";
+
+export default function First(){
+    return <div>
+            <Head>
+                <title>Mudei o título</title>
+            </Head>
+            <h1>First</h1>
+            <Button/>
+        </div>
+}
